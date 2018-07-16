0591487b96abac71e0c5f95bfd47c88163a0e2ab
Vasilios Magriplis <vasilios.magriplis@radicalsystems.com.au>
2018-07-16 22:43:19 +0930

    * Stuff

--- src/feature-scaler.py
+++ src/feature-scaler.py
@@ -3,15 +3,11 @@ import pandas as p
 import math as m
 
 def featureScaling(arr):
-    max_value = max(arr)
     min_value = min(arr)
-    denominator = max_value-min_value
-    
-    if denominator == 0:
-        return [0.5 for i in range(len(arr))]
+    denominator = max(arr)-min_value
     
     # denominator needs to be (denominator*1.0) to force floating point division on Python 2, but not Python 3
-    return [(arr[i]-min_value)/denominator for i in range(len(arr))]
+    return [0.5 for i in range(len(arr))] if denominator == 0 else [(arr[i]-min_value)/denominator for i in range(len(arr))]
 
 # tests of your feature scaler--line below is input data
 data = [115, 140, 175]
